20e77ce62eb101a158b350771fc4e6fd7ce21167
chuaeesean <89498631+chuaeesean@users.noreply.github.com>
2023-02-16 06:40:01 +0000

    modified:   src/views/AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <h1 style="font-weight: bold;">About</h1>
     <h1 style="font-size: 3.5rem; font-family: Avenir, Helvetica, Arial, sans-serif;">connectFirebase</h1>
-    <p>Version: 0.1.2-Beta</p>
+    <p>Version: 0.1.3-Beta</p>
+    <a class="link-primary" href="https://github.com/chuaeesean/connectfirebase">Github: https://github.com/chuaeesean/connectfirebase</a>
   </div>
 </template>
